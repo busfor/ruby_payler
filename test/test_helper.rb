9383efeff09584189895f6a4cee5a2fd6d47ddcc
Alexey Vasilyev <bestspajic@gmail.com>
2017-09-27 23:04:35 +0300

    Use ENV variables to control tests execution

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,22 +1,40 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+
+COVERAGE = ENV['COVERAGE']
+REFRESH_CAPYBARA = ENV['REFRESH_CAPYBARA'] || ENV['REFRESH_BY_CAPYBARA']
+REFRESH_BY_HAND = ENV['REFRESH_BY_HAND']
+
+if COVERAGE
+  require 'simplecov'
+  SimpleCov.start
+end
+
 require 'ruby_payler'
 
 require 'minitest/autorun'
-
+require 'mocha/mini_test'
+require 'capybara/minitest'
+require 'capybara/poltergeist'
 require 'pry-byebug'
 
+require 'vcr'
+VCR.configure do |config|
+  config.cassette_library_dir = 'fixtures/vcr_cassettes'
+  config.hook_into :faraday
+  config.ignore_localhost = true
+end
+
 require 'hashie'
-
-require 'capybara'
-require 'capybara/dsl'
-require 'capybara/poltergeist'
-
-require 'mocha/mini_test'
-
 CONFIG = ::Hashie::Mash.new YAML.safe_load(File.read('test/config.yml')).freeze
 
+if REFRESH_CAPYBARA || REFRESH_BY_HAND
+  FileUtils.rm_rf('fixtures/vcr_cassettes/capybara/.', secure: true)
+end
+
+# Base class for Payment flow where capybara performs payment
 class CapybaraTestCase < Minitest::Test
   include Capybara::DSL
+  include Capybara::Minitest::Assertions
 
   def pay_by_capybara(pay_url)
     card = CONFIG.card
@@ -38,9 +56,9 @@ class CapybaraTestCase < Minitest::Test
   end
 
   def perform_payment(pay_url)
-    if CONFIG.use_capybara == true
+    if REFRESH_CAPYBARA
       pay_by_capybara(pay_url)
-    else
+    elsif REFRESH_BY_HAND
       pay_by_hand(pay_url)
     end
   end
